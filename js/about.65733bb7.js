16a4592703b5c5a4087271dde4197285492935ec
nguyen <daudaua3782@gmail.com>
2020-09-23 20:22:20 +0700

    Clear all and push

--- js/about.65733bb7.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23"),e={class:"about"},o=Object(c["f"])("h1",null,"This is an about page",-1);function s(n,t,a,s,u,b){return Object(c["o"])(),Object(c["c"])("div",e,[o])}const u={};u.render=s;t["default"]=u}}]);
-//# sourceMappingURL=about.65733bb7.js.map
