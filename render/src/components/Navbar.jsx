5981c5dd88de30db1ae3df173b3d008ff802829b
Player Nguyên <daudaua3782@gmail.com>
2022-10-24 19:12:06 +0700

    Add clean up useEffect on scroll

--- render/src/components/Navbar.jsx
+++ render/src/components/Navbar.jsx
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from "react";
 
 export default function Navbar() {
   const [visible, setVisible] = useState(false);
+  
   useEffect(() => {
-    function handleScrollVisible() {}
-
-    document.addEventListener("scroll", () => {
+    function handleScrollVisible() {
       let scrollTop = document.documentElement.scrollTop;
       setVisible(scrollTop >= 200);
-    });
+    }
+
+    document.addEventListener("scroll", handleScrollVisible);
+
+    return () => {
+      document.removeEventListener("scroll", handleScrollVisible);
+    };
   }, []);
 
   return (
@@ -16,7 +21,7 @@ export default function Navbar() {
       <div
         className={`fixed top-0 px-9 py-12 bg-transparent w-full z-50 ${
           visible ? "block" : "hidden"
-        }`}
+        } text-white`}
       >
         <h1>Back to top</h1>
       </div>
