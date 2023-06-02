548a2cc807c3cedd073e998a9e3d18ce45a52734
PlayerNguyen <daudaua3782@gmail.com>
2023-06-02 20:05:40 +0700

    feat: add new Yarn, using Vite

--- render/src/index.tsx
+++ render/src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
-import "./index.css";
-import App from "./src/pages/App";
-
+console.log(`Rendering application`);
 const app = document.getElementById("app");
+if (app === null) {
+  throw new Error(`Invalid initial #app section (#app is null)`);
+}
 
 createRoot(app).render(<App />);
