969a440e756be0d05ede6a8455c2869197fe69aa
PlayerNguyen <daudaua3782@gmail.com>
2023-06-02 20:46:37 +0700

    feat: enhance and install styles utilities

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <div className="p-6">Hello world</div>;
+}
---
+++ src/Index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./Index.css";
+
+const app = document.getElementById("app");
+
+if (app === null) {
+  throw new Error(`Invalid #app element (#app could be null).`);
+}
+
+createRoot(app).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
