0365862c691b8bbbf5d22d258d222dfb31095fae
PlayerNguyen <daudaua3782@gmail.com>
2023-06-03 18:07:15 +0700

    build: add react build for vite, add react-router-dom

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+  ],
   build: {
     outDir: "./dist",
   },
